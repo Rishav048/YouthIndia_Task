937019b7eaf73c1ee858ef65b79e02b23a9c13d6
Rishav048 <chakrabortyrishav048@gmail.com>
2023-06-03 14:08:12 +0530

    task

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,16 +2,23 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Homepage from "./Pages/Homepage";
 import Product from "./Pages/Product";
-import LazyProduct from "./Pages/LazyProduct.jsx";
+import Loader from "./Component/Loader";
 
-//const LazyProduct = React.lazy(() => import("./Pages/LazyProduct.jsx"));
+const LazyProduct = React.lazy(() => import("./Pages/LazyProduct.jsx"));
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/product" element={<Product />} />
-      <Route path="/lazyproduct" element={<LazyProduct />} />
+      <Route
+        path="/lazyproduct"
+        element={
+          <React.Suspense fallback={<Loader />}>
+            <LazyProduct />
+          </React.Suspense>
+        }
+      />
     </Routes>
   );
 }
